b01e06241573b9a4aae2ae0574416151e2c80a8e
huzixuan1 <1017190168@qq.com>
2024-08-14 21:21:58 +0800

    fix issues of UT and CMakeLists

---
+++ tests/src/executeUT.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+
+/**
+ * The executeUT.cpp is the begin of UT execute.
+ */
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
