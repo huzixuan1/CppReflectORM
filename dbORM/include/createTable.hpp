4fbf45577999b3161dc4ba566d7daa110c296aec
huzixuan1 <1017190168@qq.com>
2024-10-28 19:43:21 +0800

    adjust the code

--- dbORM/include/createTable.hpp
+++ dbORM/include/createTable.hpp
@@ -8,6 +8,8 @@
 #include <boost/pfr/core_name.hpp>
 #include <boost/core/type_name.hpp>
 
+// The code needs to improved
+
 // Abstract base class for building SQL tables
 
 class TableBuilder {
@@ -51,9 +53,62 @@ public:
                    const std::string& foreignKeyColumn = "");
 };
 
+
 // Function template to get field information from a struct
 template<typename T>
 std::vector<std::pair<std::string, std::string>> getStructFieldsInfo(const T& struct_t);
+
+// Implementations of MySQLTableBuilder methods
+void MySQLTableBuilder::setTableName(const std::string& tableName) {
+    sql += "CREATE TABLE " + tableName + " (";
+}
+
+void MySQLTableBuilder::addColumn(const std::string& columnName, const std::string& columnType) {
+    sql += columnName + " " + columnType + ",";
+}
+
+void MySQLTableBuilder::setPrimaryKey(const std::string& primaryKey) {
+    sql += "PRIMARY KEY (" + primaryKey + "),";
+}
+
+void MySQLTableBuilder::addForeignKey(const std::string& columnName, const std::string& foreignKey) {
+    sql += "FOREIGN KEY (" + columnName + ") REFERENCES " + foreignKey + ",";
+}
+
+void MySQLTableBuilder::addIndex(const std::string& columnName) {
+    sql += "INDEX (" + columnName + "),";
+}
+
+std::string MySQLTableBuilder::build() {
+    sql.pop_back(); // Remove the last comma
+    sql += ");";
+    return sql;
+}
+
+void MySQLTableBuilder::reset() {
+    sql = "";
+}
+
+// Implementations of TableDirector methods
+TableDirector::TableDirector(std::shared_ptr<TableBuilder> builder) : builder(std::move(builder)) {}
+
+void TableDirector::construct(const std::string& tableName,
+                              const std::vector<std::pair<std::string, std::string>>& columns,
+                              const std::string& primaryKey,
+                              const std::string& foreignKey,
+                              const std::string& foreignKeyColumn) {
+    builder->reset();
+    builder->setTableName(tableName);
+    for (const auto& column : columns) {
+        builder->addColumn(column.first, column.second);
+    }
+    if (!primaryKey.empty()) {
+        builder->setPrimaryKey(primaryKey);
+    }
+    if (!foreignKey.empty()) {
+        builder->addForeignKey(foreignKey, foreignKeyColumn);
+    }
+}
 
 
 #endif  // TABLE_BUILDER_HPP
