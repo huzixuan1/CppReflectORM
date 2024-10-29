46620a5e41c58c4a4e689c28c35530ea5e3d119f
huzixuan1 <1017190168@qq.com>
2024-10-29 14:43:10 +0800

    add result for update.cpp

--- examples/update.cpp
+++ examples/update.cpp
@@ -14,8 +14,8 @@ int main()
     Condition cond1("age", ">", "25");
     Condition cond2("name", "=", "Bob");
 
-    auto updateSql1 = update(user, cond1,"32", "Bob");
-    auto updateSql2 = update(user, cond2,"32", "TaoTao");
+    auto updateSql1 = update(user, cond1,"32", "Bob");  // UPDATE UserInfo SET age = '32', name = 'Bob' WHERE age > '25';
+    auto updateSql2 = update(user, cond2,"32", "TaoTao");   // UPDATE UserInfo SET age = '32', name = 'TaoTao' WHERE name = 'Bob';
     std::cout << updateSql1 << std::endl; 
     std::cout << updateSql2 << std::endl; 
 
