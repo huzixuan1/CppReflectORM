1d5631b55a3895416008a9e13d11d45cf3c95495
huzixuan1 <1017190168@qq.com>
2024-10-30 10:25:20 +0800

    Optimize insert function to support insertion according to the original definition of data.

--- dbORM/include/insertAction.hpp
+++ dbORM/include/insertAction.hpp
@@ -5,13 +5,13 @@
 #include <sstream>
 #include <vector>
 #include <string>
-#include <boost/pfr.hpp> 
+#include <boost/pfr.hpp>
 
 namespace insertAction
 {
-    // insert into tableName (id,weight,salary,userName) values (1,50,230,tom);
-    template<typename T>
-    std::string insert(const T& struct_)
+    // insert into tableName (id,weight,salary,userName) values (1,50,230,'tom');
+    template <typename T>
+    std::string insert(const T &struct_)
     {
         std::ostringstream sql;
         sql << "INSERT INTO " << utility::get_table_name<T>() << " (";
@@ -28,19 +28,41 @@ namespace insertAction
         }
         sql << ") VALUES (";
 
-        boost::pfr::for_each_field(struct_, [&sql, index = 0](const auto &field) mutable {
-            if constexpr (std::is_same_v<std::decay_t<decltype(field)>, std::string>)
-                sql << "'" << field << "'";
+        // boost::pfr::for_each_field(struct_, [&sql, index = 0](const auto &field) mutable
+        //                            {
+        // using FieldType = std::decay_t<decltype(field)>;
+        // if constexpr (std::is_integral<FieldType>::value || std::is_floating_point<FieldType>::value) {
+        //     sql << field;
+        // } else {
+        //     sql << "'" << field << "'";
+        // }
+
+        // if (++index < boost::pfr::tuple_size<T>())
+        //     sql << ", "; });
+
+        // sql << ");";
+        // return sql.str();
+
+        boost::pfr::for_each_field(struct_, [&sql, index = 0](const auto &field) mutable
+                                   {
+            using FieldType = std::decay_t<decltype(field)>;
+
+            if constexpr (std::is_same_v<FieldType, std::string>)
+            {
+                sql << "'" << field << "'";  // string
+            }
             else
-                sql << "'" << std::to_string(field) << "'";
+            {
+                sql << field;  // no-string
+            }
 
             if (++index < boost::pfr::tuple_size<T>())
-                sql << ", ";
-        });
+                sql << ", "; });
 
         sql << ");";
         return sql.str();
     }
+
 }
 
 #endif // INSERT_ACTION_HPP
